7e82951482e620c5cb9ab32cbaeed2019f919eb6
rajalishikh <sakibahmed9678@gmail.com>
2026-01-12 18:00:58 +0600

    ok

--- resources/views/FrontEnd/include/checkout.blade.php
+++ resources/views/FrontEnd/include/checkout.blade.php
@@ -479,8 +479,8 @@
                         </svg>
                         <input id="name" name="name" value="{{ Auth::user()->name ?? old('name') }}" type="text" class="form-control" placeholder="আপনার নাম" required>
                         @error('name')
-                                                <div class="text-danger small mt-1">{{ $message }}</div>
-                                            @enderror
+                        <div class="text-danger small mt-1">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
 
@@ -490,7 +490,7 @@
                         <svg class="input-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                         </svg>
-                        <input type="tel" class="form-control" placeholder="ফোন নম্বর" required>
+                        <input type="number" id="phone" name="phone" class="form-control" placeholder="ফোন নম্বর" value="{{ Auth::user()->phone ?? old('phone') }}" required>
                     </div>
                 </div>
 
@@ -501,7 +501,10 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                         </svg>
-                        <input type="text" class="form-control" placeholder="ঠিকানা" required>
+                        <input type="text" name="address" id="address" class="form-control" placeholder="ঠিকানা" required>
+                        @error('address')
+                        <div class="text-danger small mt-1">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
 
@@ -620,7 +623,7 @@
 <script>
     let appliedCoupon = null;
 
-    
+
 
     function closeCheckout() {
         document.getElementById('checkoutModal').classList.remove('open');
